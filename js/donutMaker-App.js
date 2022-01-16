b14e71dcd77977d036799185dcbb6cb10c2764f9
Madison Mitchell <madisonrosemitchell@gmail.com>
2022-01-15 22:59:02 -0500

    Complete Feature 1-4; working on Feature 5 but unable to get the _donutCount to add into total _donutCount in constructor

--- js/donutMaker-App.js
+++ js/donutMaker-App.js
@@ -1,24 +1,64 @@
 import DonutMaker from "./donutMaker.js"
 
 const buttonDonut = document.querySelector("#donutButton"); //Actual Donut Image & Button
-const paraDonutCount = document.querySelector(".donutCount"); //Paragraph Element in HTML that lists Donut Count
+const buttonAutoClicker = document.querySelector("#addAutoClickerButton");
+const headerDonutCount = document.querySelector("#donutCountHeader"); //Paragraph Element in HTML that lists Donut Count
+const headerAutoClickerCount = document.querySelector("#autoClickerCountHeader") //Paragraph Element in HTML that lists Auto Clicker Count
+const headerAutoClickerPrice = document.querySelector("#autoClickerPriceHeader") //Paragraph Element in HTML that lists Auto Clicker Count
+
+const textDonutCountNumber = document.createElement("text"); //Creates text to go inside the headerDonutCount Element with the Donut Count
+const textAutoClickerNumber = document.createElement("text"); //Creates text to go inside the headerAutoClickerCount Element with the Auto Clicker Count
+const textAutoClickerPrice = document.createElement("text"); //Creates text to go inside the headerAutoClickerPrice Element with the Auto Clicker Count
+
+
 
 let donutMaker = new DonutMaker(); //Constructor
+renderPage();
 
-const textDonutCountNumber = document.createElement("text"); //Creates text to go inside the paraDonutCount Element with the Donut Count
-const currentDonutCount = donutMaker.getDonutCount(); //Grabs Donut Count and updates it to current count with any new clicks/multipliers
 
-textDonutCountNumber.innerText = currentDonutCount; //Updates the text to go inside the paraDonutCount Element with the current Donut Count 
 
-textDonutCountNumber.innerText = currentDonutCount; //Set the inner text of the Text Element to the current number of Donuts
-paraDonutCount.appendChild(textDonutCountNumber); //Attaches the Text Element of the number of Donuts to the Paragraph Element in HTML that lists Donut Count
+function renderPage() {
+    updateDonutCount();
+    addAutoClicker();
+}
+
+function updateDonutCount() {
+    const currentDonutCount = donutMaker.getDonutCount(); //Grabs Donut Count from Constructor and updates it to current count with any new clicks/multipliers
+    textDonutCountNumber.innerText = currentDonutCount; //Set the inner text of the Text Element to the current number of Donuts
+    headerDonutCount.appendChild(textDonutCountNumber); //Attaches the Text Element of the number of Donuts to the Paragraph Element in HTML that lists Donut Count
+}
+
+function addAutoClicker() { //Updates Auto Clicker Count on page
+    const currentAutoClickerCount = donutMaker.getAutoClickerCount(); //Grabs Auto Clicker Count and updates it to current count
+    const currentAutoClickerPrice = donutMaker.getAutoClickerPrice(); //Grabs Auto Clicker Price and updates it to current count
+    textAutoClickerNumber.innerText = currentAutoClickerCount;
+    textAutoClickerPrice.innerText = currentAutoClickerPrice;
+    headerAutoClickerCount.appendChild(textAutoClickerNumber); //Attaches the Text Element of the number of Auto Clickers to the Paragraph Element in HTML that lists Auto Clicker Count
+    headerAutoClickerPrice.appendChild(textAutoClickerPrice); //Attaches the Text Element of the number of Auto Clickers to the Paragraph Element in HTML that lists Auto Clicker Count
+}
+
+function buyAutoClicker() { 
+    donutMaker.buyAutoClicker(); //Pays for Auto Clicker, adds 1 and increases price
+    addDonutByAutoClicker();
+}
+
+function addDonutByAutoClicker() {
+    donutMaker.addDonutByAutoClicker();
+}
+
+function clickDonut() {
+    donutMaker.clickDonut(); //Runs function for when the Donut button is clicked to make X number of donuts
+}
+
 
 
 buttonDonut.addEventListener("click", () => {
-    donutMaker.clickDonut(); //Runs function for when the Donut button is clicked to make X number of donuts
-
-    const currentDonutCount = donutMaker.getDonutCount(); //Grabs Donut Count and updates it to current count with any new clicks/multipliers
-
-    textDonutCountNumber.innerText = currentDonutCount; //Set the inner text of the Text Element to the current number of Donuts
-    paraDonutCount.appendChild(textDonutCountNumber); //Attaches the Text Element of the number of Donuts to the Paragraph Element in HTML that lists Donut Count
+    clickDonut();
+    updateDonutCount();
 });
+
+buttonAutoClicker.addEventListener("click", () => {
+    buyAutoClicker();
+    updateDonutCount();
+    addAutoClicker();
+})
--- js/donutMaker.js
+++ js/donutMaker.js
@@ -1,17 +1,54 @@
-const donutCount = document.querySelector(".donutCount");
+const donutCount = document.querySelector("#donutCount");
 
 class DonutMaker {
     constructor() {
-        this._donutCount = 0;
+        this._donutCount = 2000;
+        this._autoClickerCount = 0;
+        this._autoClickerPrice = 100;
+        this._autoClickerDonuts = 0;
     }
 
     getDonutCount() {
-        return this._donutCount;
+        return Math.floor(this._donutCount);
+    }
+
+    getAutoClickerCount() {
+        return this._autoClickerCount;
+    }
+
+    getAutoClickerPrice() {
+        return (this._autoClickerPrice).toFixed(2);
+    }
+
+    getMultiplier() {
+        return this._multiplier;
+    }
+
+    getMultiplierPrice() {
+        return this._multiplierPrice;
     }
 
     clickDonut() {
         this._donutCount += 1;
-        console.log(this._donutCount);
+    }
+
+    addDonutByAutoClicker() { //NOT WORKING; Not adding to actual donut count in constructor
+        if (this._autoClickerCount >= 1) {
+            setInterval(() => {
+                this._donutCount += this._autoClickerCount;
+                console.log('auto click count is ' + this._autoClickerCount);
+                console.log('donut count is ' + this._donutCount);
+                console.log('auto price is ' + this._autoClickerPrice);
+            }, 1000);
+        }
+    }
+
+    buyAutoClicker() {
+        if (this._donutCount >= this._autoClickerPrice) {
+            this._donutCount -= this._autoClickerPrice;
+            this._autoClickerCount += 1;
+            this._autoClickerPrice *= 1.1;
+        }
     }
 }
 
